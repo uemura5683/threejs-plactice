53ff3b2014df6955b10a07e23dddcf51dadbf46a
uemuragame5683 <nobuyoshi5683@gmail.com>
2020-10-14 23:22:07 +0900

    現段階をコミット

--- moon/js/index.js
+++ moon/js/index.js
@@ -13,9 +13,12 @@
         });
         renderer.setPixelRatio(window.devicePixelRatio);
         renderer.setSize(width, height);
+        renderer.setClearColor(0x0E2255);
+        renderer.shadowMap.enabled = true;
 
         // シーンを作成
         const scene = new THREE.Scene();
+        scene.fog = new THREE.Fog(0xffff00, 50, 2000);
 
         // カメラを作成
         const camera = new THREE.PerspectiveCamera(45, width / height);
@@ -33,11 +36,11 @@
 
         function createStarField() {
           // 形状データを作成
-          const s_geometry = new THREE.SphereGeometry();
+          const s_geometry = new THREE.SphereGeometry( 5, 32, 32 );
           // 配置する範囲
           const SIZE = 3000;
           // 配置する個数
-          const LENGTH = 1000;
+          const LENGTH = 500;
           for (let i = 0; i < LENGTH; i++) {
             s_geometry.vertices.push(
               new THREE.Vector3(
@@ -50,7 +53,7 @@
           // マテリアルを作成
           const s_material = new THREE.PointsMaterial({
             // 一つ一つのサイズ
-            size: 5,
+            size: 10,
             // 色
             color: 0xffffff
           });
@@ -63,7 +66,7 @@
 
 
         // 平行光源
-        const directionalLight = new THREE.SpotLight(0xffffcc, 1, 0, 100, 2, 100);
+        const directionalLight = new THREE.SpotLight(0xffffff, 1, 0, 100, 2, 100);
         directionalLight.position.set(2000, 2000, 2000);
         // シーンに追加
         scene.add(directionalLight);
@@ -84,8 +87,8 @@
           camera.position.z = 1500 * Math.cos(radian);
 
           // 1秒で45°回転する
-          m_box.rotation.x = 1500 * (Math.PI / 4);
-          m_box.rotation.z = 1500 * (Math.PI / 4);
+          m_box.rotation.x = 500 * (Math.PI / 1);
+          m_box.rotation.z = 500 * (Math.PI / 1);
 
 
           // 原点方向を見つめる
