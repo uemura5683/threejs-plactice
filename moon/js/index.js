6ad7b42f4d349f36d863543336261139da2406db
uemuragame5683 <nobuyoshi5683@gmail.com>
2020-10-20 00:17:26 +0900

    地球を作ってみた

--- moon/js/index.js
+++ moon/js/index.js
@@ -5,7 +5,7 @@
         // サイズを指定
         const width = 960;
         const height = 540;
-        let rot = 0; // 角度
+        let rot = 0;
 
         // レンダラーを作成
         const renderer = new THREE.WebGLRenderer({
@@ -18,21 +18,27 @@
 
         // シーンを作成
         const scene = new THREE.Scene();
-        scene.fog = new THREE.Fog(0xffffaa, 50, 4000);
 
         // カメラを作成
         const camera = new THREE.PerspectiveCamera(45, width / height);
-        camera.position.set(0, 0, 2000);
+        camera.position.set(400, 350, 1000);
+
+        // 地球を作成
+        const e_Geometry = new THREE.SphereGeometry(200, 64, 64);
+        const e_texture = new THREE.TextureLoader().load('img/earch.jpg');
+        const e_materials = new THREE.MeshStandardMaterial( { color: 0xffffff, map:e_texture } );
+        const e_box = new THREE.Mesh(e_Geometry, e_materials);
+        scene.add(e_box);
 
         // 月を作成
-        const m_Geometry = new THREE.SphereGeometry(400, 40, 40);
+        const m_Geometry = new THREE.SphereGeometry(34, 64, 64);
         const m_texture = new THREE.TextureLoader().load('img/moon.jpg');
-        const m_materials = new THREE.MeshStandardMaterial( { map:m_texture } );
+        const m_materials = new THREE.MeshStandardMaterial( { color: 0xffffff, map:m_texture } );
         const m_box = new THREE.Mesh(m_Geometry, m_materials);
         scene.add(m_box);
 
         // 雲を作成
-        const c_texture = new THREE.TextureLoader().load('https://82mou.github.io/threejs/img/crowd.png');
+        const c_texture = new THREE.TextureLoader().load('img/crowd.png');
         const c_materials = new THREE.MeshStandardMaterial( { map:c_texture, transparent: true, side: THREE.DoubleSide } );
         const c_box = new THREE.Mesh(m_Geometry, c_materials);
         scene.add(c_box);
@@ -73,8 +79,8 @@
 
 
         // 平行光源
-        const directionalLight = new THREE.DirectionalLight(0xffffff, 0.6);
-        directionalLight.position.set(100, 130, 80);
+        const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+        directionalLight.position.set(1000, 1300, 80);
         // シーンに追加
         scene.add(directionalLight);
 
@@ -87,21 +93,30 @@
 
         // 毎フレーム時に実行されるループイベントです
         function tick() {
-          rot += 1;
+          rot += .5;
 
           // レンダリング
           renderer.render(scene, camera);
 
           // ラジアンに変換する
-          const radian = (rot * Math.PI) / 400;
+          const radian = (rot * Math.PI) / 320;
+
+          camera.position.x = 1000 * Math.sin(radian);
+          camera.position.z = 1000 * Math.cos(radian);
+
+          const radian_m = (rot * Math.PI) / 60;
+
           // 角度に応じてカメラの位置を設定
-          camera.position.x = 1500 * Math.sin(radian);
-          camera.position.z = 1500 * Math.cos(radian);
+          m_box.position.x = 300 * Math.sin(radian_m);
+          m_box.position.y = 50;
+          m_box.position.z = 300 * Math.cos(radian_m);
+
 
           // 1秒で45°回転する
-          m_box.rotation.x = 500 * (Math.PI / 1);
-          m_box.rotation.z = 500 * (Math.PI / 1);
-          c_box.rotation.x = 5000 * (Math.PI / 1);
+          e_box.rotation.x = 500 * (Math.PI / 1);
+          e_box.rotation.z = 500 * (Math.PI / 1);
+          c_box.rotation.x = 8000 * (Math.PI / 1);
+
 
           // 原点方向を見つめる
           camera.lookAt(new THREE.Vector3(0, 0, 0));
