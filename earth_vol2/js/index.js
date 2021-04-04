1c57f6b7109f545319caa10526a363cd6845caba
uemuragame5683 <nobuyoshi5683@gmail.com>
2021-04-05 00:51:19 +0900

    jsを修正

--- earth_vol2/js/index.js
+++ earth_vol2/js/index.js
@@ -16,29 +16,29 @@ function init() {
 
   const scene = new THREE.Scene();
 
-  const camera = new THREE.PerspectiveCamera( 45, width / height );
-  camera.position.set( 400, 350, 1000 );
+  const camera = new THREE.PerspectiveCamera( 45, width / height, 0.1, 10000 );
+  camera.position.set( 400, 350, 5000 );
 
   const e_Geometry = new THREE.SphereGeometry( 200, 64, 64 );
-  const e_texture = new THREE.TextureLoader().load('https://raw.githubusercontent.com/uemura5683/threejs_plactice/master/moon/img/earch.jpg');
+  const e_texture = new THREE.TextureLoader().load('https://raw.githubusercontent.com/uemura5683/threejs_plactice/master/earth_vol2/img/earch.jpg');
   const e_materials = new THREE.MeshStandardMaterial( { color: 0xffffff, map:e_texture } );
   const e_box = new THREE.Mesh(e_Geometry, e_materials );
   scene.add(e_box);
 
   const m_Geometry = new THREE.SphereGeometry( 34, 64, 64 );
-  const m_texture = new THREE.TextureLoader().load('https://raw.githubusercontent.com/uemura5683/threejs_plactice/master/moon/img/moon.jpg');
+  const m_texture = new THREE.TextureLoader().load('https://raw.githubusercontent.com/uemura5683/threejs_plactice/master/earth_vol2/img/moon.jpg');
   const m_materials = new THREE.MeshStandardMaterial( { color: 0xffffff, map:m_texture } );
   const m_box = new THREE.Mesh( m_Geometry, m_materials );
   scene.add(m_box);
 
   const c_Geometry = new THREE.SphereGeometry( 205, 64, 64 );
-  const c_texture = new THREE.TextureLoader().load('https://raw.githubusercontent.com/uemura5683/threejs_plactice/master/moon/img/crowd.png');
+  const c_texture = new THREE.TextureLoader().load('https://raw.githubusercontent.com/uemura5683/threejs_plactice/master/earth_vol2/img/crowd.png');
   const c_materials = new THREE.MeshStandardMaterial( { map:c_texture, transparent: true, side: THREE.DoubleSide } );
   const c_box = new THREE.Mesh( c_Geometry, c_materials  );
   scene.add(c_box);
 
   const s_Geometry = new THREE.SphereGeometry( 205, 64, 64 );
-  const s_texture = new THREE.TextureLoader().load('https://raw.githubusercontent.com/uemura5683/threejs_plactice/master/moon/img/moon.jpg');
+  const s_texture = new THREE.TextureLoader().load('https://raw.githubusercontent.com/uemura5683/threejs_plactice/master/earth_vol2/img/sun.jpg');
   const s_materials = new THREE.MeshStandardMaterial( { color: 0xffffff, map:s_texture } );
   const s_box = new THREE.Mesh( s_Geometry, s_materials );
   scene.add(s_box);
@@ -74,6 +74,7 @@ function init() {
   scene.add(lightd);
 
 
+
   tick();
 
   function tick() {
@@ -81,7 +82,7 @@ function init() {
 
     renderer.render( scene, camera );
 
-    const t = Date.now() / 5000;
+    const t = Date.now() / 1000;
     const r = 3000.0;
     const lx = r * Math.cos(t);
     const lz = r * Math.sin(t);
@@ -102,11 +103,10 @@ function init() {
     m_box.position.y = 50;
     m_box.position.z = 300 * Math.cos( m_radian );
 
-    const s_radian = ( rot * Math.PI ) / 100;
-    s_box.position.x = 1000 * Math.sin( s_radian );
+    const s_radian = - (( rot * Math.PI ) / 1000);
+    s_box.position.x = 5000 * Math.sin( s_radian );
     s_box.position.y = 50;
-    s_box.position.z = 1000 * Math.cos( s_radian );
-
+    s_box.position.z = 5000 * Math.cos( s_radian );
 
     e_box.rotation.x = 500 * ( Math.PI / 1 );
     e_box.rotation.z = 500 * ( Math.PI / 1 );
