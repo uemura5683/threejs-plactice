49f057f50899b9347f64a893890ef91839a1d05c
uemuragame5683 <nobuyoshi5683@gmail.com>
2021-05-21 23:01:41 +0900

    0521分コミット

--- diamond/js/index.js
+++ diamond/js/index.js
@@ -25,13 +25,8 @@ function init() {
 
 
   // 光の輪を作成
-  let c_texture = new THREE.TextureLoader().load('img/swirl.png');
   let material = new THREE.MeshBasicMaterial({
-    map: c_texture,
-    color: 0x96f4ff,
-    transparent: true,
-    blending: THREE.AdditiveBlending,
-    side: THREE.DoubleSide
+    color: 0x88FFFF
   });
 
   let geometry = new THREE.TorusGeometry( (Math.random() + .5) * 400, 10, 2, 200);
@@ -39,231 +34,69 @@ function init() {
   mesh.rotation.set(Math.random() * 2, Math.random() * 2, Math.random() * 2);
   scene.add(mesh);
 
-  let geometry2 = new THREE.TorusGeometry( (Math.random() + .55) * 400, 10, 2, 200);
-
+  let geometry2 = new THREE.TorusGeometry( (Math.random() + .55) * 400, 12, 2, 200);
   let mesh2 = new THREE.Mesh(geometry2, material)
   mesh2.rotation.set(Math.random() * 2, Math.random() * 2, Math.random() * 2);
   scene.add(mesh2);
 
-  let geometry3 = new THREE.TorusGeometry( (Math.random() + .6) * 400, 10, 2, 200);
+  let geometry3 = new THREE.TorusGeometry( (Math.random() + .6) * 400, 14, 2, 200);
   let mesh3 = new THREE.Mesh(geometry3, material)
   mesh3.rotation.set(Math.random() * 2, Math.random() * 2, Math.random() * 2);
   scene.add(mesh3);
 
-  let geometry4 = new THREE.TorusGeometry( (Math.random() + .65) * 400, 10, 2, 200);
+  let geometry4 = new THREE.TorusGeometry( (Math.random() + .65) * 400, 16, 2, 200);
   let mesh4 = new THREE.Mesh(geometry4, material)
   mesh4.rotation.set(Math.random() * 2, Math.random() * 2, Math.random() * 2);
   scene.add(mesh4);
 
-  let geometry5 = new THREE.TorusGeometry( (Math.random() + .7) * 400, 10, 2, 200);
+  let geometry5 = new THREE.TorusGeometry( (Math.random() + .7) * 400, 18, 2, 200);
   let mesh5 = new THREE.Mesh(geometry5, material)
   mesh5.rotation.set(Math.random() * 2, Math.random() * 2, Math.random() * 2);
   scene.add(mesh5);
-
-  // 光を作成
-  let sp_texture = new THREE.TextureLoader().load('img/particle.png');
 
   const group = new THREE.Group();
   // 3D空間にグループを追加する
   scene.add(group);
 
-  for (let i = 0; i < 200; i++) {
-    let sp_material = new THREE.SpriteMaterial({
-      color: 0x96f4ff,
-      map: sp_texture,
-      transparent: true,
-      blending: THREE.AdditiveBlending
-    });
-    let sprite = new THREE.Sprite(sp_material);
-    // ランダムな座標に配置
-    sprite.position.x = 1000 * (Math.random() - 0.5);
-    sprite.position.y = 1000 * (Math.random() - 0.5);
-    sprite.position.z = 1000 * (Math.random() - 0.5);
-    // 必要に応じてスケールを調整
-    sprite.scale.set(30, 30, 30);
-    group.add(sprite);
+  for (let i = 0; i < 300; i++) {
+    let geometry = new THREE.SphereBufferGeometry(4, 3, 4),
+        size = 1;
+    let m_material = new THREE.MeshPhongMaterial({
+      color: 0xCBF5FD,
+      flatShading: true
+    })
+    const mesh = new THREE.Mesh(geometry, m_material)
+    mesh.position.set(size * Math.random() - 0.5, size * Math.random() - 0.5, size * Math.random() - 0.5).normalize()
+    mesh.position.multiplyScalar(Math.random() * 800)
+    mesh.rotation.set(Math.random() * 4, Math.random() * 2, Math.random() * 2)
+    mesh.scale.x = mesh.scale.y = mesh.scale.z = Math.random() * 2
+    group.add(mesh)
   }
 
-  var cristal;
-  function createCristal() {
-    var dark_color = 0x007a8c;
-    var light_color = 0x96f4ff;
-    var x_left = 70;
-    var x_right = -x_left;
-    var x_middle = x_right - x_right;
-    var y_bottom = 20;
-    var y_middle = y_bottom + 150;
-    var y_top = y_middle + 90;
-    var z_back = 70;
-    var z_front = -z_back;
-    var z_middle = z_front - z_front;
-  
-    // 表面用のマテリアル生成
-    var material_normal = new THREE.MeshPhongMaterial({
-      vertexColors: THREE.VertexColors,
-      specular: 0xffffff,
-      shininess: 10,
-      transparent: true,
-      opacity: 0.85
-    });
-    // 裏面用のマテリアル生成
-    var material_back = new THREE.MeshPhongMaterial({
-      side: THREE.BackSide,
-      vertexColors: THREE.VertexColors,
-      specular: 0xffffff,
-      shininess: 10,
-      transparent: true,
-      opacity: 0.85
-    });
-  
-    // クリスタルオブジェクト生成
-    cristal = new THREE.Object3D();
-  
-    // 1つ目のオブジェクト作成
-    var front_left_top_g = new THREE.Geometry();
-    front_left_top_g.vertices = [
-      new THREE.Vector3(x_left, y_middle, z_middle),
-      new THREE.Vector3(x_middle, y_top, z_middle),
-      new THREE.Vector3(x_middle, y_middle, z_front)
-    ];
-    var front_left_top_c = [
-      new THREE.Color(dark_color),
-      new THREE.Color(light_color),
-      new THREE.Color(dark_color)
-    ];
-    front_left_top_g.faces[0] = new THREE.Face3(0, 1, 2, null, front_left_top_c);
-    front_left_top_g.computeFaceNormals();
-    var front_left_top = new THREE.Mesh(front_left_top_g, material_back);
-    cristal.add(front_left_top);
-  
-    // 2つ目のオブジェクト作成
-    var back_left_top_g = new THREE.Geometry();
-    back_left_top_g.vertices = [
-      new THREE.Vector3(x_left, y_middle, z_middle),
-      new THREE.Vector3(x_middle, y_top, z_middle),
-      new THREE.Vector3(x_middle, y_middle, z_back)
-    ];
-    var back_left_top_c = [
-      new THREE.Color(dark_color),
-      new THREE.Color(light_color),
-      new THREE.Color(dark_color)
-    ];
-    back_left_top_g.faces[0] = new THREE.Face3(0, 1, 2, null, back_left_top_c);
-    back_left_top_g.computeFaceNormals();
-    
-    var back_left_top = new THREE.Mesh(back_left_top_g, material_normal);
-    cristal.add(back_left_top);
+  let c_geometry = new THREE.Geometry();
+  c_geometry.vertices.push(new THREE.Vector3(0, 0, 100));
+  c_geometry.vertices.push(new THREE.Vector3(100, 0, 0));
+  c_geometry.vertices.push(new THREE.Vector3(0, -200, 0));
+  c_geometry.vertices.push(new THREE.Vector3(-100, 0, 0));
+  c_geometry.vertices.push(new THREE.Vector3(0, 100, 0));
+  c_geometry.vertices.push(new THREE.Vector3(0, 0, -100));
+  c_geometry.faces.push(new THREE.Face3( 0, 2, 1));
+  c_geometry.faces.push(new THREE.Face3( 0, 3, 2));
+  c_geometry.faces.push(new THREE.Face3( 0, 4, 3));
+  c_geometry.faces.push(new THREE.Face3( 0, 1, 4));
+  c_geometry.faces.push(new THREE.Face3( 5, 1, 2));
+  c_geometry.faces.push(new THREE.Face3( 5, 2, 3));
+  c_geometry.faces.push(new THREE.Face3( 5, 3, 4));
+  c_geometry.faces.push(new THREE.Face3( 5, 4, 1));
+  c_geometry.computeFaceNormals();
+  c_geometry.computeVertexNormals();
+  let c_material = new THREE.MeshPhongMaterial({color: 0x88FFFF});
+  let c_mesh = new THREE.Mesh(c_geometry, c_material);
+  scene.add(c_mesh);
 
-    // 3つ目のオブジェクト作成
-    var front_right_top_g = new THREE.Geometry();
-    front_right_top_g.vertices = [
-      new THREE.Vector3(x_right, y_middle, z_middle),
-      new THREE.Vector3(x_middle, y_top, z_middle),
-      new THREE.Vector3(x_middle, y_middle, z_front)
-    ];
-    var front_right_top_c = [
-      new THREE.Color(dark_color),
-      new THREE.Color(light_color),
-      new THREE.Color(dark_color)
-    ];
-    front_right_top_g.faces[0] = new THREE.Face3(0, 1, 2, null, front_right_top_c);
-    front_right_top_g.computeFaceNormals();
-    var front_right_top = new THREE.Mesh(front_right_top_g, material_back);
-    cristal.add(front_right_top);
-  
-    // 4つ目のオブジェクト作成
-    var back_right_top_g = new THREE.Geometry();
-    back_right_top_g.vertices = [
-      new THREE.Vector3(x_right, y_middle, z_middle),
-      new THREE.Vector3(x_middle, y_top, z_middle),
-      new THREE.Vector3(x_middle, y_middle, z_back)
-    ];
-    var back_right_top_c = [
-      new THREE.Color(dark_color),
-      new THREE.Color(light_color),
-      new THREE.Color(dark_color)
-    ];
-    back_right_top_g.faces[0] = new THREE.Face3(0, 1, 2, null, back_right_top_c);
-    back_right_top_g.computeFaceNormals();
-    
-    var back_right_top = new THREE.Mesh(back_right_top_g, material_normal);
-    cristal.add(back_right_top);
-
-    // 5つ目のオブジェクト作成
-    var front_left_bottom_g = new THREE.Geometry();
-    front_left_bottom_g.vertices = [
-      new THREE.Vector3(x_left, y_middle, z_middle),
-      new THREE.Vector3(x_middle, y_bottom, z_middle),
-      new THREE.Vector3(x_middle, y_middle, z_front)
-    ];
-    var front_left_bottom_c = [
-      new THREE.Color(dark_color),
-      new THREE.Color(light_color),
-      new THREE.Color(dark_color)
-    ];
-    front_left_bottom_g.faces[0] = new THREE.Face3(0, 1, 2, null, front_left_bottom_c);
-    front_left_bottom_g.computeFaceNormals();
-    var front_left_bottom = new THREE.Mesh(front_left_bottom_g, material_back);
-    cristal.add(front_left_bottom);
-  
-    // 6つ目のオブジェクト作成
-    var back_left_bottom_g = new THREE.Geometry();
-    back_left_bottom_g.vertices = [
-      new THREE.Vector3(x_left, y_middle, z_middle),
-      new THREE.Vector3(x_middle, y_bottom, z_middle),
-      new THREE.Vector3(x_middle, y_middle, z_back)
-    ];
-    var back_left_bottom_c = [
-      new THREE.Color(dark_color),
-      new THREE.Color(light_color),
-      new THREE.Color(dark_color)
-    ];
-    back_left_bottom_g.faces[0] = new THREE.Face3(0, 1, 2, null, back_left_bottom_c);
-    back_left_bottom_g.computeFaceNormals();
-    
-    var back_left_bottom = new THREE.Mesh(back_left_bottom_g, material_normal);
-    cristal.add(back_left_bottom);
-
-    // 7つ目のオブジェクト作成
-    var front_right_bottom_g = new THREE.Geometry();
-    front_right_bottom_g.vertices = [
-      new THREE.Vector3(x_right, y_middle, z_middle),
-      new THREE.Vector3(x_middle, y_bottom, z_middle),
-      new THREE.Vector3(x_middle, y_middle, z_front)
-    ];
-    var front_right_bottom_c = [
-      new THREE.Color(dark_color),
-      new THREE.Color(light_color),
-      new THREE.Color(dark_color)
-    ];
-    front_right_bottom_g.faces[0] = new THREE.Face3(0, 1, 2, null, front_right_bottom_c);
-    front_right_bottom_g.computeFaceNormals();
-    var front_right_bottom = new THREE.Mesh(front_right_bottom_g, material_back);
-    cristal.add(front_right_bottom);
-  
-    // 8つ目のオブジェクト作成
-    var back_right_bottom_g = new THREE.Geometry();
-    back_right_bottom_g.vertices = [
-      new THREE.Vector3(x_right, y_middle, z_middle),
-      new THREE.Vector3(x_middle, y_bottom, z_middle),
-      new THREE.Vector3(x_middle, y_middle, z_back)
-    ];
-    var back_right_bottom_c = [
-      new THREE.Color(dark_color),
-      new THREE.Color(light_color),
-      new THREE.Color(dark_color)
-    ];
-    back_right_bottom_g.faces[0] = new THREE.Face3(0, 1, 2, null, back_right_bottom_c);
-    back_right_bottom_g.computeFaceNormals();
-    
-    var back_right_bottom = new THREE.Mesh(back_right_bottom_g, material_normal);
-    cristal.add(back_right_bottom);
-
-    cristal.position.set(0, -200, 0);
-    scene.add(cristal);
-  }
-
-  createCristal();
+  let wire = new THREE.MeshBasicMaterial({color: 0xffffff, wireframe: true});
+  let wireMesh = new THREE.Mesh(c_geometry,wire);//同じ形状からワイヤーも作成
+  scene.add(wireMesh);
 
   // 平行光源
   const directionalLight = new THREE.DirectionalLight(0xfffffff);
@@ -278,8 +111,9 @@ function init() {
 
   var cristal_step = 0;
   function rotatemesh() {
-    // cristal_step += 0.02;
-    // cristal.rotation.set(0, cristal_step, 0);
+    cristal_step += 0.02;
+    c_mesh.rotation.set(0, cristal_step, 0);
+    wireMesh.rotation.set(0, cristal_step, 0);
     mesh.rotation.x = 1;
     mesh.rotation.y += 0.01;
     mesh2.rotation.x += 0.01;
