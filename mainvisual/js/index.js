6b3a059806470e07fe747650be2235e2ab2b984e
uemuragame5683 <nobuyoshi5683@gmail.com>
2021-09-20 01:09:20 +0900

    途中までコミット

--- mainvisual/js/index.js
+++ mainvisual/js/index.js
@@ -8,30 +8,43 @@
     let width = window.innerWidth,
         height = window.innerHeight;
 
-    // レンダラーを作成
+    /**
+    * renderer
+    **/
     const renderer = new THREE.WebGLRenderer({
       canvas: document.querySelector('#myCanvas')
     });
     renderer.setPixelRatio(window.devicePixelRatio);
     renderer.setSize(width, height);
 
-    // シーンを作成
+    /**
+    * scene
+    **/
     const scene = new THREE.Scene();
 
-    // カメラを作成
+    /**
+    * camera
+    **/
     const camera = new THREE.PerspectiveCamera(45, width / height);
     camera.position.set(0, 0, +1000);
 
-    // 箱を作成
-    // const geometry = new THREE.BoxGeometry(400, 400, 400);
-    // const material = new THREE.MeshNormalMaterial();
-    // const box = new THREE.Mesh(geometry, material);
-    // scene.add(box);
+    /**
+    * controls
+    **/
+    const controls = new THREE.OrbitControls(camera, document.body);
+
+    /**
+    * box
+    **/
+    const geometry = new THREE.SphereBufferGeometry(200, 200, 200);
+    const material = new THREE.MeshNormalMaterial();
+    const box = new THREE.Mesh(geometry, material);
+    scene.add(box);
 
     tick();
 
     function tick() {
-      // box.rotation.y += 0.01;
+      box.rotation.y += 0.01;
       renderer.render(scene, camera);
       requestAnimationFrame(tick);
     }
