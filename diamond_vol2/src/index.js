1b308ba674c3512a03d08416e32cce1fd9f33b7c
uemuragame5683 <nobuyoshi5683@gmail.com>
2021-11-11 00:04:17 +0900

    diamond_vol2を追加

---
+++ diamond_vol2/src/index.js
@@ -0,0 +1,156 @@
+const canvasSketch = require("canvas-sketch");
+
+const THREE = require("three");
+global.THREE = THREE;
+
+// Import extra THREE plugins
+require("three/examples/js/controls/OrbitControls");
+require("three/examples/js/geometries/RoundedBoxGeometry.js");
+require("three/examples/js/loaders/GLTFLoader.js");
+
+const Stats = require("stats-js");
+const { GUI } = require("dat.gui");
+
+const settings = {
+  animate: true,
+  context: "webgl",
+  resizeCanvas: false,
+};
+
+const sketch = ({ context, canvas }) => {
+  const stats = new Stats();
+  document.body.appendChild(stats.dom);
+  const gui = new GUI();
+
+  const options = {
+    enableSwoopingCamera: false,
+    enableRotation: true,
+    transmission: 1,
+    thickness: 1,
+    roughness: 0,
+  };
+
+  // Setup
+  // -----
+
+  const renderer = new THREE.WebGLRenderer({ context, alpha: true });
+  renderer.setClearColor(0xa9bcf2, 1);
+
+  const camera = new THREE.PerspectiveCamera(45, 1, 0.01, 100);
+  camera.position.set(0, 0, 5);
+
+  const controls = new THREE.OrbitControls(camera, canvas);
+  controls.enabled = !options.enableSwoopingCamera;
+
+  const scene = new THREE.Scene();
+
+  const positions = [
+    [0, 0, 0],
+  ];
+
+  const geometries = [
+    new THREE.IcosahedronGeometry(0.75, 0), // Faceted
+  ];
+
+  const material = new THREE.MeshPhysicalMaterial({
+    transmission: options.transmission,
+    thickness: options.thickness,
+    roughness: options.roughness,
+  });
+
+  const meshes = geometries.map(
+    (geometry) => new THREE.Mesh(geometry, material)
+  );
+
+  meshes.forEach((mesh, i) => {
+    scene.add(mesh);
+    mesh.position.set(...positions[i]);
+  });
+
+
+  // GUI
+  // ---
+
+  gui.add(options, "enableSwoopingCamera").onChange((val) => {
+    controls.enabled = !val;
+    controls.reset();
+  });
+
+  gui.add(options, "enableRotation").onChange(() => {
+    meshes.forEach((mesh) => mesh.rotation.set(0, 0, 0));
+  });
+
+  gui.add(options, "transmission", 0, 1, 0.01).onChange((val) => {
+    material.transmission = val;
+  });
+
+  gui.add(options, "thickness", 0, 5, 0.1).onChange((val) => {
+    material.thickness = val;
+  });
+
+  gui.add(options, "roughness", 0, 1, 0.01).onChange((val) => {
+    material.roughness = val;
+  });
+
+  // Update
+  // ------
+
+  const update = (time, deltaTime) => {
+    const ROTATE_TIME = 30; // Time in seconds for a full rotation
+    const xAxis = new THREE.Vector3(1, 0, 0);
+    const yAxis = new THREE.Vector3(0, 1, 0);
+    const rotateX = (deltaTime / ROTATE_TIME) * Math.PI * 2;
+    const rotateY = (deltaTime / ROTATE_TIME) * Math.PI * 2;
+
+    if (options.enableRotation) {
+      meshes.forEach((mesh) => {
+        mesh.rotateOnWorldAxis(xAxis, rotateX);
+        mesh.rotateOnWorldAxis(yAxis, rotateY);
+      });
+    }
+
+    if (options.enableSwoopingCamera) {
+      camera.position.x = Math.sin((time / 10) * Math.PI * 2) * 2;
+      camera.position.y = Math.cos((time / 10) * Math.PI * 2) * 2;
+      camera.position.z = 4;
+      camera.lookAt(scene.position);
+    }
+  };
+
+  // Lifecycle
+  // ---------
+
+  return {
+    resize({ canvas, pixelRatio, viewportWidth, viewportHeight }) {
+      const dpr = Math.min(pixelRatio, 2); // Cap DPR scaling to 2x
+
+      canvas.width = viewportWidth * dpr;
+      canvas.height = viewportHeight * dpr;
+      canvas.style.width = viewportWidth + "px";
+      canvas.style.height = viewportHeight + "px";
+
+      renderer.setPixelRatio(dpr);
+      renderer.setSize(viewportWidth, viewportHeight);
+
+      camera.aspect = viewportWidth / viewportHeight;
+      camera.updateProjectionMatrix();
+    },
+    render({ time, deltaTime }) {
+      stats.begin();
+      controls.update();
+      update(time, deltaTime);
+      renderer.render(scene, camera);
+      stats.end();
+    },
+    unload() {
+      geometries.forEach((geometry) => geometry.dispose());
+      material.dispose();
+      controls.dispose();
+      renderer.dispose();
+      gui.destroy();
+      document.body.removeChild(stats.dom);
+    },
+  };
+};
+
+canvasSketch(sketch, settings);
