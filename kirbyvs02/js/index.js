6e5c50c1f8178997fefa056993cba92fc258ce56
uemuragame5683 <nobuyoshi5683@gmail.com>
2022-01-16 01:18:28 +0900

    リファクタリング

--- kirbyvs02/js/index.js
+++ kirbyvs02/js/index.js
@@ -7,7 +7,7 @@ let scene    = null,
     camera   = null,
     renderer = null,
     controls = null,
-    bat      = null,
+    zerotwo  = null,
     width    = 0,
     height   = 0;
 
@@ -17,12 +17,9 @@ let scene    = null,
 function init() {
   width = window.innerWidth,
   height = window.innerHeight;
-  
   scene = new THREE.Scene();
-
   camera = new THREE.PerspectiveCamera(45, width / height);
   camera.position.set(0, 160, 1100);
-
   renderer = new THREE.WebGLRenderer({
     alpha: true
   });
@@ -32,12 +29,10 @@ function init() {
   renderer.setSize(width, height);
   renderer.shadowMap.enabled = true;
   renderer.shadowMap.type = THREE.PCFSoftShadowMap;
-  
   controls = new THREE.OrbitControls(camera, renderer.domElement);
   
   addLights();
-  // drawCoffin();
-  drawBat();
+  drawZerotwo();
 
   document.getElementById('myCanvas').appendChild(renderer.domElement);
   window.addEventListener('resize',onResize,false);
@@ -51,7 +46,6 @@ function addLights() {
   directLight1.castShadow = true;
   directLight1.position.set(0, 1, 1);
   scene.add(directLight1);
-
   const pointLight = new THREE.PointLight(0xffffff, 2, 1000);
   scene.add(pointLight);
 }
@@ -59,9 +53,9 @@ function addLights() {
 /**
 * draw
 **/
-function drawBat() {
-  bat = new Bat();
-  scene.add(bat.group);
+function drawZerotwo() {
+  zerotwo = new Zerotwo();
+  scene.add(zerotwo.group);
 }
 
 /**
@@ -95,15 +89,14 @@ function animate() {
 **/
 function render() {
   //scene.rotation.y += 0.01;
-  bat.moveBody();
-  bat.moveWings();
+  zerotwo.moveBody();
   renderer.render(scene, camera);
 }
 
 /**
-* Bat
+* Zerotwo
 **/
-class Bat {
+class Zerotwo {
   constructor() {
     this.group = new THREE.Group();
     this.group.position.set(0, 0, 0);
@@ -111,18 +104,16 @@ class Bat {
     this.wingangle = 0;
     this.bodyangle = 0;
     this.drawBody();
-    this.drawWings();
-    this.drawWings_left();
   }
 
   drawBody() {
     /**
     * body
     **/
-    const kirby_body = new THREE.SphereBufferGeometry(200, 200, 200);
-    const kirby_body_material = new THREE.MeshBasicMaterial({color: 0xFFFFFF});
-    const kirby_box = new THREE.Mesh(kirby_body, kirby_body_material);
-    this.group.add(kirby_box);
+    const body_geometry = new THREE.SphereBufferGeometry(200, 200, 200);
+    const body_material = new THREE.MeshBasicMaterial({color: 0xFFFFFF});
+    const body          = new THREE.Mesh(body_geometry, body_material);
+    this.group.add(body);
 
     /**
     * ring
@@ -137,58 +128,57 @@ class Bat {
     /**
     * eye
     **/
-    const eye =  new THREE.ParametricGeometry( function( u, v, target ) {
-                            u = u * Math.PI;
-                            v = v * 4 * Math.PI;
-                            var x = 30 * Math.sin(u) * Math.cos(v);
-                            var y = 60 * Math.sin(u) * Math.sin(v); 
-                            var z = 1 * Math.cos(u);
-                            target.set( x, y, z );
-                        }, 80, 80, true
-                   );
-
+    const eye_geometry =  new THREE.ParametricGeometry( function( u, v, target ) {
+                              u = u * Math.PI;
+                              v = v * 4 * Math.PI;
+                              var x = 30 * Math.sin(u) * Math.cos(v);
+                              var y = 60 * Math.sin(u) * Math.sin(v); 
+                              var z = 1 * Math.cos(u);
+                              target.set( x, y, z );
+                            }, 80, 80, true
+                          );
     const eyes_material = new THREE.MeshBasicMaterial({color: 0xEE1A2B});
 
-    const eyes_box = new THREE.Mesh(eye, eyes_material);
-    eyes_box.rotation.set(0, 0, degree(90));
-    eyes_box.position.set(0, 10, 200);
-    this.group.add(eyes_box);
+    const eye = new THREE.Mesh(eye_geometry, eyes_material);
+    eye.rotation.set(0, 0, degree(90));
+    eye.position.set(0, 10, 200);
+    this.group.add(eye);
 
     /**
     * eye black
     **/
-    const eye_black = new THREE.ParametricGeometry( function( u, v, target ) {
-                            u = u * Math.PI;
-                            v = v * 2 * Math.PI;
-                            var x = 20 * Math.sin(u) * Math.cos(v);
-                            var y = 20 * Math.sin(u) * Math.sin(v); 
-                            var z = 1 * Math.cos(u);
-                            target.set( x, y, z );
-                        }, 100, 100, true
-                   );
+    const eye_black_geometry = new THREE.ParametricGeometry( function( u, v, target ) {
+                                    u = u * Math.PI;
+                                    v = v * 2 * Math.PI;
+                                    var x = 20 * Math.sin(u) * Math.cos(v);
+                                    var y = 20 * Math.sin(u) * Math.sin(v); 
+                                    var z = 1 * Math.cos(u);
+                                    target.set( x, y, z );
+                                  }, 100, 100, true
+                                );
 
     const eyes_black_material = new THREE.MeshBasicMaterial({color: 0x000000});
-    const eyes_black_box = new THREE.Mesh(eye_black, eyes_black_material);
-    eyes_black_box.position.set(0, 10, 204);
-    this.group.add(eyes_black_box);
+    const eyes_black = new THREE.Mesh(eye_black_geometry, eyes_black_material);
+    eyes_black.position.set(0, 10, 204);
+    this.group.add(eyes_black);
 
     /**
     * eye white
     **/
-    const eye_white = new THREE.ParametricGeometry( function( u, v, target ) {
-                            u = u * Math.PI;
-                            v = v * 2 * Math.PI;
-                            var x = 10 * Math.sin(u) * Math.cos(v);
-                            var y = 10 * Math.sin(u) * Math.sin(v); 
-                            var z = 1 * Math.cos(u);
-                            target.set( x, y, z );
-                        }, 100, 100, true
-                   );
+    const eye_white_material = new THREE.ParametricGeometry( function( u, v, target ) {
+                                    u = u * Math.PI;
+                                    v = v * 2 * Math.PI;
+                                    var x = 10 * Math.sin(u) * Math.cos(v);
+                                    var y = 10 * Math.sin(u) * Math.sin(v); 
+                                    var z = 1 * Math.cos(u);
+                                    target.set( x, y, z );
+                                  }, 100, 100, true
+                                );
 
     const eyes_white_material = new THREE.MeshBasicMaterial({color: 0xFFFFFF});
-    const eyes_white_box = new THREE.Mesh(eye_white, eyes_white_material);
-    eyes_white_box.position.set(10, 10, 207);
-    this.group.add(eyes_white_box);
+    const eyes_white = new THREE.Mesh(eye_white_material, eyes_white_material);
+    eyes_white.position.set(10, 10, 207);
+    this.group.add(eyes_white);
 
     /**
     * foot
@@ -196,29 +186,32 @@ class Bat {
     const tail_geometry = new THREE.ConeGeometry( 50, 100, 50 );
     const tail_material = new THREE.MeshBasicMaterial( {color: 0xFFFFFF} );
     const tail = new THREE.Mesh( tail_geometry, tail_material );
-    tail.position.set(0,-220, 0);
+    tail.position.set(0, -220, 0);
     tail.rotation.set(0, 0, degree(180));
     this.group.add( tail );
 
-  }
-  drawWings() {
     const splash_material = new THREE.MeshPhongMaterial({color: 0xffffff})
         , shape = new THREE.Shape();
-          shape.moveTo(0, 30);
+          shape.moveTo(5, 30);
           shape.lineTo(52, -40);
-          shape.lineTo(28, -60);
-          shape.lineTo(0, -60);
-          shape.lineTo(-28, -40);
-          shape.lineTo(0, 30);
+          shape.lineTo(32, -60);
+          shape.lineTo(6, -60);
+          shape.lineTo(-14, -40);
+          shape.lineTo(5, 30);
     const splash_geometry = new THREE.ShapeGeometry(shape),
           splash_box = new THREE.Mesh(splash_geometry, splash_material);
     /**
-    * rightwing
+    *twing
     **/
     this.rightWing = splash_box;
     this.rightWing.position.set(200, 0, 0);
-    this.rightWing.rotation.set(degree(180), 0, degree(-90));
+    this.rightWing.rotation.set(0, 0, degree(90));
     this.group.add(this.rightWing);
+
+    this.rightWing_right = this.rightWing.clone();
+    this.rightWing_right.position.set(-200, 40, 0);
+    this.rightWing_right.rotation.set(0, 0, degree(-90));
+    this.group.add(this.rightWing_right);
 
     const wing_material = new THREE.MeshPhongMaterial({color: 0xffffff})
         , shape_wing = new THREE.Shape();
@@ -231,187 +224,113 @@ class Bat {
     const wing_geometry = new THREE.ShapeGeometry(shape_wing),
           wing_box = new THREE.Mesh(wing_geometry, wing_material);
 
-    /**
-    * rightwing
-    **/
     this.wing_box = wing_box;
-    this.wing_box.position.set(280, 80, 0);
-    this.wing_box.rotation.set(degree(180), 0, degree(-45));
+    this.wing_box.position.set(340, 140, 0);
+    this.wing_box.rotation.set(0, 0, degree(-45));
     this.group.add(this.wing_box);
 
+    this.wing_box_right = this.wing_box.clone();
+    this.wing_box_right.position.set(-340, 140, 0);
+    this.wing_box_right.rotation.set(0, 0, degree(45));
+    this.group.add(this.wing_box_right);
+
     this.wing_box_second = this.wing_box.clone();
-    this.wing_box_second.position.set(310, 20, 0);
-    this.wing_box_second.rotation.set(degree(180), 0, degree(-90));
+    this.wing_box_second.position.set(390, 20, 0);
+    this.wing_box_second.rotation.set(0, 0, degree(-90));
     this.group.add(this.wing_box_second);
 
+    this.wing_box_second_right = this.wing_box.clone();
+    this.wing_box_second_right.position.set(-390, 20, 0);
+    this.wing_box_second_right.rotation.set(0, 0, degree(90));
+    this.group.add(this.wing_box_second_right);
+
     this.wing_box_third = this.wing_box.clone();
-    this.wing_box_third.position.set(290, -40, 0);
-    this.wing_box_third.rotation.set(degree(180), 0, degree(-135));
+    this.wing_box_third.position.set(340, -100, 0);
+    this.wing_box_third.rotation.set(0, 0, degree(-135));
     this.group.add(this.wing_box_third);
 
+    this.wing_box_third_right = this.wing_box.clone();
+    this.wing_box_third_right.position.set(-340, -100, 0);
+    this.wing_box_third_right.rotation.set(0, 0, degree(135));
+    this.group.add(this.wing_box_third_right);
 
     const wing_red_material = new THREE.MeshPhongMaterial({color: 0x97042C})
         , shape_wing_red = new THREE.Shape();
-          shape_wing_red.moveTo(   7,  30  );
-          shape_wing_red.lineTo(  15, -45  );
-          shape_wing_red.lineTo(   7, -115 );
-          shape_wing_red.lineTo(  -7, -115 );
-          shape_wing_red.lineTo( -15, -45  );
-          shape_wing_red.lineTo(  -7,  30  );
+          shape_wing_red.moveTo(7,30);
+          shape_wing_red.lineTo(15,-45);
+          shape_wing_red.lineTo(7,-115);
+          shape_wing_red.lineTo(-7,-115);
+          shape_wing_red.lineTo(-15,-45);
+          shape_wing_red.lineTo(-7,30);
     const wing_red_geometry = new THREE.ShapeGeometry(shape_wing_red),
           wing_red = new THREE.Mesh(wing_red_geometry, wing_red_material);
 
     this.wing_red = wing_red;
-    this.wing_red.position.set(370, 210, 0);
-    this.wing_red.rotation.set(degree(180), 0, degree(-22.5));
+    this.wing_red.position.set(400, 290, 0);
+    this.wing_red.rotation.set(0, 0, degree(-22.5));
     this.group.add(this.wing_red);
 
+    this.wing_red_right = this.wing_red.clone();
+    this.wing_red_right.position.set(-400, 290, 0);
+    this.wing_red_right.rotation.set(0, 0, degree(22.5));
+    this.group.add(this.wing_red_right);
+
     this.wing_red_two = wing_red.clone();
-    this.wing_red_two.position.set(410, 180, 0);
-    this.wing_red_two.rotation.set(degree(180), 0, degree(-53));
+    this.wing_red_two.position.set(470, 230, 0);
+    this.wing_red_two.rotation.set(0, 0, degree(-53));
     this.group.add(this.wing_red_two);
 
+    this.wing_red_two_right = wing_red.clone();
+    this.wing_red_two_right.position.set(-470, 230, 0);
+    this.wing_red_two_right.rotation.set(0, 0, degree(53));
+    this.group.add(this.wing_red_two_right);
+
     this.wing_red_three = wing_red.clone();
-    this.wing_red_three.position.set(470, 50, 0);
-    this.wing_red_three.rotation.set(degree(180), 0, degree(-70));
+    this.wing_red_three.position.set(540, 80, 0);
+    this.wing_red_three.rotation.set(0, 0, degree(-70));
     this.group.add(this.wing_red_three);
 
+    this.wing_red_three_right = wing_red.clone();
+    this.wing_red_three_right.position.set(-540, 80, 0);
+    this.wing_red_three_right.rotation.set(0, 0, degree(70));
+    this.group.add(this.wing_red_three_right);
+
     this.wing_red_four = wing_red.clone();
-    this.wing_red_four.position.set(470, -10, 0);
-    this.wing_red_four.rotation.set(degree(180), 0, degree(-110));
+    this.wing_red_four.position.set(540, -30, 0);
+    this.wing_red_four.rotation.set(0, 0, degree(-110));
     this.group.add(this.wing_red_four);
 
+    this.wing_red_four_right = wing_red.clone();
+    this.wing_red_four_right.position.set(-540, -30, 0);
+    this.wing_red_four_right.rotation.set(0, 0, degree(110));
+    this.group.add(this.wing_red_four_right);
+
     this.wing_red_five = wing_red.clone();
-    this.wing_red_five.position.set(420, -140, 0);
-    this.wing_red_five.rotation.set(degree(180), 0, degree(-120));
+    this.wing_red_five.position.set(480, -180, 0);
+    this.wing_red_five.rotation.set(0, 0, degree(-120));
     this.group.add(this.wing_red_five);
 
+    this.wing_red_five_right = wing_red.clone();
+    this.wing_red_five_right.position.set(-480, -180, 0);
+    this.wing_red_five_right.rotation.set(0, 0, degree(120));
+    this.group.add(this.wing_red_five_right);
+
     this.wing_red_six = wing_red.clone();
-    this.wing_red_six.position.set(380, -170, 0);
-    this.wing_red_six.rotation.set(degree(180), 0, degree(-160));
+    this.wing_red_six.position.set(400, -250, 0);
+    this.wing_red_six.rotation.set(0, 0, degree(-160));
     this.group.add(this.wing_red_six);
-  }
-  drawWings_left() {
-    const splash_material = new THREE.MeshPhongMaterial({color: 0xffffff})
-        , shape = new THREE.Shape();
-          shape.moveTo(0, 30);
-          shape.lineTo(52, -40);
-          shape.lineTo(28, -60);
-          shape.lineTo(0, -60);
-          shape.lineTo(-28, -40);
-          shape.lineTo(0, 30);
-    const splash_geometry = new THREE.ShapeGeometry(shape),
-          splash_box = new THREE.Mesh(splash_geometry, splash_material);
-    /**
-    * rightwing
-    **/
-    this.leftWing = splash_box;
-    this.leftWing.position.set(-200, 20, 0);
-    this.leftWing.rotation.set(degree(180), 0, degree(80));
-    this.group.add(this.leftWing);
 
-    const wing_material = new THREE.MeshPhongMaterial({color: 0xffffff})
-        , shape_wing = new THREE.Shape();
-          shape_wing.moveTo(15, 30);
-          shape_wing.lineTo(30, -45);
-          shape_wing.lineTo(15, -115);
-          shape_wing.lineTo(-15, -115);
-          shape_wing.lineTo(-30, -45);
-          shape_wing.lineTo(-15, 30);
-    const wing_geometry = new THREE.ShapeGeometry(shape_wing),
-          wing_box = new THREE.Mesh(wing_geometry, wing_material);
+    this.wing_red_six_right = wing_red.clone();
+    this.wing_red_six_right.position.set(-400, -250, 0);
+    this.wing_red_six_right.rotation.set(0, 0, degree(160));
+    this.group.add(this.wing_red_six_right);
 
-    /**
-    * rightwing
-    **/
-    this.wing_left_box = wing_box;
-    this.wing_left_box.position.set(-280, 80, 0);
-    this.wing_left_box.rotation.set(degree(180), 0, degree(45));
-    this.group.add(this.wing_left_box);
-
-    this.wing_left_box_second = this.wing_box.clone();
-    this.wing_left_box_second.position.set(-310, 20, 0);
-    this.wing_left_box_second.rotation.set(degree(180), 0, degree(90));
-    this.group.add(this.wing_left_box_second);
-
-    this.wing_left_box_third = this.wing_box.clone();
-    this.wing_left_box_third.position.set(-290, -40, 0);
-    this.wing_left_box_third.rotation.set(degree(180), 0, degree(135));
-    this.group.add(this.wing_left_box_third);
-
-
-    const wing_red_material = new THREE.MeshPhongMaterial({color: 0x97042C})
-        , shape_wing_red = new THREE.Shape();
-          shape_wing_red.moveTo(   7,  30  );
-          shape_wing_red.lineTo(  15, -45  );
-          shape_wing_red.lineTo(   7, -115 );
-          shape_wing_red.lineTo(  -7, -115 );
-          shape_wing_red.lineTo( -15, -45  );
-          shape_wing_red.lineTo(  -7,  30  );
-    const wing_red_geometry = new THREE.ShapeGeometry(shape_wing_red),
-          wing_red = new THREE.Mesh(wing_red_geometry, wing_red_material);
-
-    this.wing_left_red = wing_red;
-    this.wing_left_red.position.set(-370, 210, 0);
-    this.wing_left_red.rotation.set(degree(180), 0, degree(22.5));
-    this.group.add(this.wing_left_red);
-
-    this.wing_left_red_two = wing_red.clone();
-    this.wing_left_red_two.position.set(-410, 180, 0);
-    this.wing_left_red_two.rotation.set(degree(180), 0, degree(53));
-    this.group.add(this.wing_left_red_two);
-
-    this.wing_left_red_three = wing_red.clone();
-    this.wing_left_red_three.position.set(-470, 50, 0);
-    this.wing_left_red_three.rotation.set(degree(180), 0, degree(70));
-    this.group.add(this.wing_left_red_three);
-
-    this.wing_left_red_four = wing_red.clone();
-    this.wing_left_red_four.position.set(-470, -10, 0);
-    this.wing_left_red_four.rotation.set(degree(180), 0, degree(110));
-    this.group.add(this.wing_left_red_four);
-
-    this.wing_left_red_five = wing_red.clone();
-    this.wing_left_red_five.position.set(-420, -140, 0);
-    this.wing_left_red_five.rotation.set(degree(180), 0, degree(120));
-    this.group.add(this.wing_left_red_five);
-
-    this.wing_left_red_six = wing_red.clone();
-    this.wing_left_red_six.position.set(-380, -170, 0);
-    this.wing_left_red_six.rotation.set(degree(180), 0, degree(160));
-    this.group.add(this.wing_left_red_six);
   }
   moveBody() {
     const bodyamplitude = 30;
     this.bodyangle += 0.05;
     this.group.position.y = 0 - (Math.cos(this.bodyangle) * bodyamplitude);
   }  
-  moveWings() {
-    this.wingangle += .05;
-    const wingamplitude            = degree(20);
-    // this.rightWing.rotation.y      = degree(180) - (Math.cos(this.wingangle) * wingamplitude);
-    this.rightWing.rotation.y
-  = this.wing_box.rotation.y
-  = this.wing_box_second.rotation.y
-  = this.wing_box_third.rotation.y
-  = this.wing_red.rotation.y
-  = this.wing_red_two.rotation.y
-  = this.wing_red_three.rotation.y
-  = this.wing_red_four.rotation.y
-  = this.wing_red_five.rotation.y
-  = this.wing_red_six.rotation.y
-  = this.leftWing.rotation.y
-  = this.wing_left_box.rotation.y
-  = this.wing_left_box_second.rotation.y
-  = this.wing_left_box_third.rotation.y
-  = this.wing_left_red.rotation.y
-  = this.wing_left_red_two.rotation.y
-  = this.wing_left_red_three.rotation.y
-  = this.wing_left_red_four.rotation.y
-  = this.wing_left_red_five.rotation.y
-  = this.wing_left_red_six.rotation.y
-  = degree(180);
-  }
 }
 
 init();
